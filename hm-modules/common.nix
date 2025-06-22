403040acf63bab72ee75425e0c273e6f345dc998
Alex <skardizone@gmail.com>
2025-06-23 02:43:56 +0300

    refactor(hm): modularize alex user config

--- hm-modules/common.nix
+++ hm-modules/common.nix
@@ -1,13 +1,15 @@
+# /home/alex/nixos-config/hm-modules/common.nix
 { pkgs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
   home.stateVersion = "25.11";
 
+  # Оверлей для rycee больше не нужен, так как мы используем
+  # встроенную опцию для языковых пакетов. Убираем его.
+  # nixpkgs.overlays = [ ... ];
+
   home.packages = with pkgs; [
     telegram-desktop
     obsidian
   ];
-
-  # Мы удалили отсюда `programs.git.enable = true;`,
-  # так как Git теперь является персональной настройкой.
 }
