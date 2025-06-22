95147157367639561b8b21705bc0ce4b583e57c3
Alex <skardizone@gmail.com>
2025-06-22 18:15:58 +0300

    feat: final SSH setup and testing

--- hm-modules/alex.nix
+++ hm-modules/alex.nix
@@ -1,23 +1,20 @@
 { pkgs, ... }:
 {
-  # Удаляем home.language.base, так как будем использовать более прямой метод
-
   home.packages = with pkgs; [ firefox ];
-
-  programs.git = { enable = true; userName = "Alex"; userEmail = "skardizone@gmail.com"; };
-
-  # Прямое управление файлом конфигурации локали KDE
+  programs.git = {
+    enable = true;
+    userName = "Alex";
+    userEmail = "skardizone@gmail.com";
+  };
+  programs.ssh.startAgent = true;
   home.file.".config/plasma-localerc" = {
     text = ''
       [Formats]
       LANG=ru_RU.UTF-8
-
       [Translations]
       LANG=ru_RU.UTF-8
     '';
   };
-
-  # Конфигурация панели задач остается прежней
   home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".text = ''
     [Containments][2][Applets][3][Configuration][General]
     launchers=applications:systemsettings.desktop,applications:org.kde.konsole.desktop,applications:org.kde.dolphin.desktop
