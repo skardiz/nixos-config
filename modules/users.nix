ad93ed1513cea2cd3f4e7e930f09ba531253acdf
Alex <skardizone@gmail.com>
2025-06-23 02:01:10 +0300

    feat(kde): final taskbar config using plasma-manager

--- modules/users.nix
+++ modules/users.nix
@@ -1,23 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }: # Добавляем `inputs`
 {
-  # Создаем группу для управления конфигурацией
   users.groups.nixos-config = {};
-
   users.users = {
-    alex = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" "gamemode" "nixos-config" ];
-    };
-    mari = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" "gamemode" ];
-    };
+    alex = { isNormalUser = true; extraGroups = [ "wheel" "gamemode" "nixos-config" ]; };
+    mari = { isNormalUser = true; extraGroups = [ "wheel" "gamemode" ]; };
   };
-
-  # Глобальные настройки Home Manager
   home-manager = {
-    # Решает проблему конфликта файлов, создавая бэкапы
     backupFileExtension = "bak";
+    # Передаем `specialArgs` в Home Manager
+    extraSpecialArgs = { inherit inputs; };
     users = {
       alex = { imports = [ ../hm-modules/common.nix ../hm-modules/alex.nix ]; };
       mari = { imports = [ ../hm-modules/common.nix ../hm-modules/mari.nix ]; };
