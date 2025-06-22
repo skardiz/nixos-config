78486042f7dcb70ed4c7b78d5dbee60b20f6daf3
Alex <skardizone@gmail.com>
2025-06-23 02:09:42 +0300

    feat(kde): final taskbar config using plasma-manager

--- hm-modules/alex.nix
+++ hm-modules/alex.nix
@@ -18,17 +18,22 @@
     LANG=ru_RU.UTF-8
   '';
 
-  # Финальная, правильная конфигурация панели задач через plasma-manager
+  # Финальная, 100% правильная конфигурация панели задач через plasma-manager
   programs.plasma = {
     enable = true;
-    panels."Панель".widgets = [
-      "org.kde.plasma.kicker.desktop" # Пуск
-      "systemsettings.desktop"          # Настройки
-      "org.kde.konsole.desktop"         # Консоль
-      "org.kde.dolphin.desktop"         # Dolphin
-      "appimagekit_972a71f0a155c4a56973305b0797321c-obsidian.desktop" # Obsidian
-      "firefox.desktop"                 # Firefox
-      "telegramdesktop.desktop"         # Telegram
+    panels = [ # Теперь это список!
+      {
+        name = "Панель"; # Имя панели как атрибут в элементе списка
+        widgets = [
+          "org.kde.plasma.kicker.desktop" # Пуск
+          "systemsettings.desktop"          # Настройки
+          "org.kde.konsole.desktop"         # Консоль
+          "org.kde.dolphin.desktop"         # Dolphin
+          "appimagekit_972a71f0a155c4a56973305b0797321c-obsidian.desktop" # Obsidian
+          "firefox.desktop"                 # Firefox
+          "telegramdesktop.desktop"         # Telegram
+        ];
+      }
     ];
   };
 }
