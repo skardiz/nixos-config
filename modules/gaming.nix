3f3f7825d7f0c59c7b411ab8e32bbab25526cab1
Alex <skardizone@gmail.com>
2025-06-25 02:46:17 +0300

    feat: add nix-ld libraries for Hearthstone

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 {
   environment.sessionVariables = { MANGOHUD = "1"; };
-  programs.nix-ld.enable = true;
   programs.steam = {
     enable = true;
     extraCompatPackages = [ pkgs.proton-ge-bin ];
