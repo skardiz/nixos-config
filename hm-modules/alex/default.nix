202ae3634a81826ce5077227f2ac154d46a229ee
Alex <skardizone@gmail.com>
2025-06-25 21:00:54 +0300

    fix: simplify hiddify setup to working hiddify-app with easy WARP import

--- hm-modules/alex/default.nix
+++ hm-modules/alex/default.nix
@@ -1,4 +1,6 @@
 # /home/alex/nixos-config/hm-modules/alex/default.nix
+{ pkgs, config, ... }:
+
 {
   imports = [
     ./git.nix
@@ -7,4 +9,5 @@
     ./bash.nix
     ./packages.nix
   ];
+  # Убедись, что здесь нет ничего про hiddify-cli или systemd.user.services
 }
