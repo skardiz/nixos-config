11838988e38156dc060dde4eaee4d66a0822c03b
Alex <skardizone@gmail.com>
2025-06-25 03:34:38 +0300

    nvidia-drm.modeset=1

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,16 +1,17 @@
+# /home/alex/nixos-config/modules/nvidia.nix
 { config, pkgs, ... }:
+
 {
+  # Включаем DRM Kernel Mode Setting для фикса разрывов (tearing)
+  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+
   hardware.graphics = { enable = true; enable32Bit = true; };
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
-    # Меняем powerManagement.enable на true
     powerManagement.enable = true;
-    open = false; # Используем проприетарные драйверы
+    open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
-
-    # Убираем nvidiaConfig, так как powerManagement.enable должен управлять этим
-    # Если проблемы сохранятся, мы вернемся к более тонким настройкам
   };
 }
