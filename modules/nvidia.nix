29932d5b1a7b173c4a5cae7e8e3c282f0d6fa07a
Alex <skardizone@gmail.com>
2025-06-25 19:33:24 +0300

    nvidia stable

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -9,6 +9,6 @@
     powerManagement.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
