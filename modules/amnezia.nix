9dfe17839f783384843193a20a290d8d50ae16c8
Alex <skardizone@gmail.com>
2025-06-26 14:05:57 +0300

    amnezia zen

--- modules/amnezia.nix
+++ modules/amnezia.nix
@@ -8,10 +8,10 @@
   # Если у тебя Zen-ядро, используй 'linux_zen'.
   # Если стандартное ядро, возможно 'linux_latest' или 'linux_6_6' (проверь свое ядро!).
   boot.extraModulePackages = [
-    config.boot.kernelPackages.amneziawg # Это обычно сработает для стандартного ядра
+    # config.boot.kernelPackages.amneziawg # Это обычно сработает для стандартного ядра
     # ЕСЛИ У ТЕБЯ ZEN-ЯДРО (или другое специфичное), РАСКОММЕНТИРУЙ СЛЕДУЮЩУЮ СТРОКУ
     # И ЗАКОММЕНТИРУЙ ПРЕДЫДУЩУЮ (config.boot.kernelPackages.amneziawg)!
-    # pkgs.linuxKernel.packages.linux_zen.amneziawg
+    pkgs.linuxKernel.packages.linux_zen.amneziawg
   ];
 
   # 2. Устанавливаем необходимые пакеты для всех пользователей.
