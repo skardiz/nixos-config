9b742162de1c258a9417829018eca992b3996cfe
skardiz <skardizone@gmail.com>
2025-06-29 00:25:19 +0300

    Update vpn.nix

--- modules/profiles/vpn.nix
+++ modules/profiles/vpn.nix
@@ -7,12 +7,12 @@
   # Указываем конкретный пакет модуля ядра, соответствующий твоему ядру.
   # Если у тебя Zen-ядро, используй 'linux_zen'.
   # Если стандартное ядро, возможно 'linux_latest' или 'linux_6_6' (проверь свое ядро!).
-  boot.extraModulePackages = [
+  #boot.extraModulePackages = [
     # config.boot.kernelPackages.amneziawg # Это обычно сработает для стандартного ядра
     # ЕСЛИ У ТЕБЯ ZEN-ЯДРО (или другое специфичное), РАСКОММЕНТИРУЙ СЛЕДУЮЩУЮ СТРОКУ
     # И ЗАКОММЕНТИРУЙ ПРЕДЫДУЩУЮ (config.boot.kernelPackages.amneziawg)!
-    pkgs.linuxKernel.packages.linux_zen.amneziawg
-  ];
+    #pkgs.linuxKernel.packages.linux_zen.amneziawg
+  #];
 
   # 2. Устанавливаем необходимые пакеты для всех пользователей.
   environment.systemPackages = with pkgs; [
