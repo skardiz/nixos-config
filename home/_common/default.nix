2269a665f5b45c853861309de1377332027cc740
Alex <skardizone@gmail.com>
2025-06-29 13:19:02 +0300

    del common.nix

--- home/_common/default.nix
+++ home/_common/default.nix
@@ -1,9 +1,26 @@
 # home/_common/default.nix
-# Этот файл собирает все общие модули для пользователей.
+# Содержит базовые, общие для всех пользователей настройки Home Manager.
 { ... }:
 {
   imports = [
     ./git.nix
+    # Мы убрали bash.nix, так как решили использовать 'ванильный' bash
     ./waydroid-idle.nix
+  ];
+
+  # --- Базовые настройки Home Manager ---
+  nixpkgs.config.allowUnfree = true;
+  home.stateVersion = "25.11"; # Важно для стабильности
+
+  # --- Общие программы, которые действительно нужны всем ---
+  # Например, утилиты командной строки
+  home.packages = with pkgs; [
+    google-chrome
+    libreoffice
+    obsidian
+    telegram-desktop
+    tree
+    zapzap
+    zoom-us
   ];
 }
