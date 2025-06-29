817ef0a34a448fc58657177dbd904f09e26a1a3c
Alex <skardizone@gmail.com>
2025-06-29 15:35:29 +0300

    fix(core): Use robust overlay method for zlib-ng

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -4,37 +4,29 @@
 
 {
   # --- Настройки Nix ---
-  # Включаем экспериментальные возможности, необходимые для Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Настраиваем автоматическую очистку мусора для экономии места на диске
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
-
-  # Включаем автоматическую оптимизацию хранилища Nix
   nix.optimise.automatic = true;
 
+  # --- Оверлей для zlib (самый надежный метод) ---
+  # Заменяем стандартную библиотеку zlib на её высокопроизводительную
+  # версию zlib-ng с помощью низкоуровневого оверлея.
+  # Этот метод работает на всех версиях NixOS.
+  nixpkgs.config.zlib.package = pkgs.zlib-ng;
+
   # --- Загрузчик ---
-  # Используем systemd-boot, стандартный для систем с UEFI
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # --- Оптимизация производительности ---
-  # Заменяем стандартную библиотеку zlib на её высокопроизводительную
-  # версию zlib-ng. Это ускоряет многие системные операции.
-  boot.zlib.implementation = "zlib-ng";
-
-  # Включаем еженедельную команду fstrim для SSD-накопителей
   services.fstrim.enable = true;
 
   # --- Локализация и время ---
-  # Устанавливаем часовой пояс
   time.timeZone = "Europe/Moscow";
-
-  # Настраиваем системные локали
   i18n.defaultLocale = "ru_RU.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "ru_RU.UTF-8";
@@ -47,8 +39,6 @@
     LC_TELEPHONE = "ru_RU.UTF-8";
     LC_TIME = "ru_RU.UTF-8";
   };
-
-  # Настраиваем раскладку клавиатуры для консоли (TTY)
   console = {
     font = "Lat2-Terminus16";
     keyMap = "ru";
