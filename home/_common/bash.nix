80f8875e288040bdbadc1c14d16d3186c96f4f96
Alex <skardizone@gmail.com>
2025-06-29 12:50:58 +0300

    move .config

--- home/_common/bash.nix
+++ home/_common/bash.nix
@@ -1,10 +1,17 @@
 # home/_common/bash.nix
-{ ... }:
+{ config, ... }: # Убедитесь, что 'config' здесь есть
+
 {
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    historyFile = ".config/bash/history";
+
+    # БЫЛО (вероятно):
+    # historyFile = ".config/bash/history";
+
+    # СТАЛО:
+    # Указываем абсолютный путь к файлу истории в домашней директории пользователя.
+    historyFile = "${config.home.homeDirectory}/.config/bash/history";
 
     # Общие, полезные для всех алиасы
     shellAliases = {
