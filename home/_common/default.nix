4458e15e3dd097261c1defcb3472ead449080947
Alex <skardizone@gmail.com>
2025-06-29 15:42:35 +0300

    fix(config): Move allowUnfree option to system level

--- home/_common/default.nix
+++ home/_common/default.nix
@@ -1,7 +1,5 @@
 # home/_common/default.nix
 # Содержит базовые, общие для всех пользователей настройки Home Manager.
-
-# Явно просим передать нам 'pkgs' и другие инструменты
 { pkgs, ... }:
 
 {
@@ -11,11 +9,10 @@
   ];
 
   # --- Базовые настройки Home Manager ---
-  nixpkgs.config.allowUnfree = true;
+  # УДАЛЕНО: nixpkgs.config.allowUnfree = true;
   home.stateVersion = "25.11"; # Важно для стабильности
 
-  # --- Общие программы, которые действительно нужны всем ---
-  # Теперь Nix знает, что такое 'pkgs', и эта строка будет работать
+  # --- Пакеты, которые будут установлены для всех пользователей ---
   home.packages = with pkgs; [
     google-chrome
     libreoffice
