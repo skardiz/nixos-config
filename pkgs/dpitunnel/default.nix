70e3009534121601396d60baf7f86e8e40ef0f40
Alex <skardizone@gmail.com>
2025-07-01 01:36:59 +0300

    dpi-tunnel

--- pkgs/dpitunnel/default.nix
+++ pkgs/dpitunnel/default.nix
@@ -1,29 +1,31 @@
 # pkgs/dpitunnel/default.nix
 # Наш чертеж для сборки НОВОГО DPITunnel на C++.
-{ lib, stdenv, fetchFromGitHub, cmake, libnl, cpp-httplib }:
+{ lib, stdenv, fetchFromGitHub, cmake, libnl, cpp-httplib, openssl }: # <-- ДОБАВЛЯЕМ openssl, т.к. он тоже нужен
 
 stdenv.mkDerivation rec {
   pname = "DPITunnel";
-  version = "1.0.3"; # Берем последнюю версию релиза
+  version = "1.0.3";
 
   src = fetchFromGitHub {
     owner = "txtsd";
     repo = "DPITunnel";
     rev = "v${version}";
-    # Обновите хеш, если версия изменится
     hash = "sha256-11f8F/fK1i+e8V5v/w4t6X9b+p7Y9z/C6f8V9s/d7A8=";
   };
 
   # Указываем инструменты для сборки
   nativeBuildInputs = [ cmake ];
-  # Указываем зависимости, которые нужны для сборки
-  buildInputs = [ libnl cpp-httplib ];
+
+  # --- ИСПРАВЛЕНИЕ ЗДЕСЬ! ---
+  # Мы явно указываем ВСЕ зависимости, которые нужны для сборки,
+  # включая 'cpp-httplib' и его зависимость 'openssl'.
+  buildInputs = [ libnl cpp-httplib openssl ];
 
   meta = with lib; {
     description = "A C++ proxy server that uses desync attacks to bypass DPI";
     homepage = "https://github.com/txtsd/DPITunnel";
     license = licenses.gpl3Only;
     platforms = platforms.linux;
-    maintainers = [ maintainers.skardizone ]; # Вы все еще мейнтейнер!
+    maintainers = [ maintainers.skardizone ];
   };
 }
