c1ba14ffc7c37ebbc58e77307f8d373d5530a3cd
Alex <skardizone@gmail.com>
2025-07-01 02:52:04 +0300

    feat(net)!: Finalize DPITunnel implementation, remove Thorium

--- modules/features/dpi-tunnel.nix
+++ modules/features/dpi-tunnel.nix
@@ -1,19 +1,19 @@
 # modules/features/dpi-tunnel.nix
 { pkgs, ... }:
 {
-  # Включаем наш новый, кастомный пакет
-  environment.systemPackages = [ pkgs.dpitunnel ];
-
-  # Создаем сервис
+  # Создаем системный сервис, который будет запускать наш "лазерный резак".
   systemd.services.dpitunnel = {
-    description = "DPI Tunnel Proxy Service (C++ version)";
+    description = "DPI Tunnel Proxy Service";
+    # Запускаем после того, как сеть будет готова.
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
+    # Включаем при загрузке системы.
     wantedBy = [ "multi-user.target" ];
+
     serviceConfig = {
       # --- ГЛАВНАЯ МАГИЯ! ---
       # Мы используем первую из двух "магических строк", рекомендованных автором.
-      # Мы также правильно указываем путь к сертификатам.
+      # Мы также правильно указываем путь к корневым сертификатам.
       ExecStart = ''
         ${pkgs.dpitunnel}/bin/dpitunnel \
           --ca-bundle-path=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt \
@@ -26,6 +26,8 @@
           --wsize=1 \
           --wsfactor=6
       '';
+
+      # Автоматически перезапускать, если он упадет.
       Restart = "always";
       RestartSec = "10s";
     };
