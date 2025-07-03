6d0688a64faa6b11a7d93961f1b43aef1d67b2d3
Alex <skardizone@gmail.com>
2025-07-03 17:06:46 +0300

    feat(sops): Настройка sops с новым ключом age

--- hosts/shershulya/default.nix
+++ hosts/shershulya/default.nix
@@ -1,4 +1,3 @@
-# hosts/shershulya/default.nix
 { config, pkgs, inputs, lib, ... }:
 
 {
@@ -10,26 +9,20 @@
     ../../modules/hardware/intel-cpu.nix
   ];
 
-  # --- ПРОСТАЯ И НАДЕЖНАЯ КОНФИГУРАЦИЯ SOPS ---
   sops = {
-    # Мы просто указываем путь к файлу с секретами.
-    defaultSopsFile = ../../secrets.yaml;
+    # Указываем путь к приватному ключу age
+    age.keyFile = "/etc/sops/keys/sops.key";
 
-    # И мы включаем поддержку расшифровки через SSH ключи хоста.
-    # sops-nix сам найдет нужный ключ в /etc/ssh.
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-
-    # Определяем наш секрет. Пустые скобки означают "использовать
-    # настройки по умолчанию", которые нам подходят (владелец - root).
-    secrets.github_token = {};
+    # Указываем путь к зашифрованному файлу
+    secrets.github_token = {
+      sopsFile = ../../secrets.yaml;
+    };
   };
 
-  # Настройки Nix.
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     access-tokens = "github.com=${config.sops.secrets.github_token.path}";
     extra-substituters = [ "https://nix-community.cachix.org" ];
-    # Используем правильный ключ с МИНУСОМ, а не подчеркиванием.
     extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9UfP3dPH2-jeLqIphSkeUV3ZTLb61E4gD4sIC=" ];
   };
 
