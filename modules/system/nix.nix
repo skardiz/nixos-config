688e90e5d128ef354960d915d63d3a9a766c2ed2
Alex <skardizone@gmail.com>
2025-07-03 19:52:04 +0300

    key

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -13,11 +13,5 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     extra-substituters = [ "https://nix-community.cachix.org" ];
-
-    # --- ВОТ ОН, НАШ ПЕРВОРОДНЫЙ ГРЕХ. ИСПРАВЛЕН. ---
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
   };
 }
