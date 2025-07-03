0c63b47afb1edd686dd014960f5e1d2bab04c10e
Alex <skardizone@gmail.com>
2025-07-03 19:09:07 +0300

    test

--- hosts/shershulya/default.nix
+++ hosts/shershulya/default.nix
@@ -7,25 +7,23 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/roles/desktop.nix
+    # --- ВОЗВРАЩАЕМ УКРАДЕННОЕ ---
+    ../../modules/hardware/nvidia-pascal.nix
+    ../../modules/hardware/intel-cpu.nix
   ];
 
   # --- НАСТРОЙКИ, УНИКАЛЬНЫЕ ДЛЯ ЭТОГО ХОСТА ---
-
-  # SOPS: Активируем и настраиваем Sops для этого хоста
   sops = {
     age.keyFile = "/etc/sops/keys/sops.key";
     secrets.github_token.sopsFile = ../../secrets.yaml;
     secrets.vpn_private_key.sopsFile = ../../secrets.yaml;
   };
 
-  # NIX: Добавляем только ту настройку, которая зависит от sops этого хоста.
   nix.settings.access-tokens = "github.com=${config.sops.secrets.github_token.path}";
 
-  # Уникальное имя хоста и версия системы.
   networking.hostName = "shershulya";
   system.stateVersion = "25.11";
 
-  # Уникальные пользователи для этого хоста.
   my.users.accounts = {
     alex = { isMainUser = true; description = "Alex"; extraGroups = [ "adbusers" ]; };
     mari = { description = "Mari"; };
