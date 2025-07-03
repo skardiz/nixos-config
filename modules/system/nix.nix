d5b93c82f3e47bc0f061b87dc419d0fb3ffd4a00
Alex <skardizone@gmail.com>
2025-07-03 22:04:35 +0300

    feat(nix): Restore declarative cachix configuration

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -12,6 +12,15 @@
   # Здесь мы определяем сами настройки.
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
-    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    # --- ВОТ ОН, ПРАВИЛЬНЫЙ КЛЮЧ, КОТОРЫЙ МЫ ВЫСТРАДАЛИ ---
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 }
