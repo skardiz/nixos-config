6f251ec0ec1bb099002958bbdc8e39839f64cdb5
Alex <skardizone@gmail.com>
2025-07-04 17:18:11 +0300

    fix(network): Force NetworkManager to control DNS

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,36 +1,31 @@
 # modules/system/nix.nix
 # Общие настройки для Nix, которые будут применяться ко всем хостам,
-# включая стабилизацию сети.
-{ pkgs, ... }:
+# включая стабилизацию сети с высшим приоритетом.
+{ pkgs, lib, ... }: # <-- Убедись, что 'lib' здесь есть
 
 {
   # --- СТАРЫЕ, ПРАВИЛЬНЫЕ НАСТРОЙКИ NIX ---
-  # Эта опция говорит NixOS: "Возьми все настройки из блока nix.settings
-  # и сделай их глобальными, записав в /etc/nix/nix.conf".
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
 
-  # Здесь мы определяем сами настройки.
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     extra-substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
-    # --- ВОТ ОН, ПРАВИЛЬНЫЙ КЛЮЧ, КОТОРЫЙ МЫ ВЫСТРАДАЛИ ---
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
-  # --- НОВЫЙ БЛОК ДЛЯ СТАБИЛИЗАЦИИ СЕТИ ---
-  # Эта настройка говорит NetworkManager: "Независимо от того, что тебе
-  # скажет роутер по DHCP, всегда используй вот эти надежные DNS-серверы".
-  # Это стабилизирует DNS-разрешение во время пересборки.
-  networking.networkmanager.dns = "default";
-  networking.nameservers = [
+  # --- НОВЫЙ, УСИЛЕННЫЙ БЛОК ДЛЯ СЕТИ ---
+  # С помощью `lib.mkForce` мы отдаем приказ с высшим приоритетом,
+  # отменяя любые конфликтующие настройки по умолчанию.
+  networking.networkmanager.dns = lib.mkForce "default";
+  networking.nameservers = lib.mkForce [
     "1.1.1.1"  # Cloudflare DNS
     "8.8.8.8"  # Google DNS
     "9.9.9.9"  # Quad9 DNS (еще один для надежности)
