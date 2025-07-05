cddf5464b51152f3c5bdd2dc7868bd89f6b09654
Alex <skardizone@gmail.com>
2025-07-05 03:12:42 +0300

    test(filebrowser): Use neutral ground for volume

--- modules/features/filebrowser.nix
+++ modules/features/filebrowser.nix
@@ -1,29 +1,19 @@
-# modules/features/filebrowser.nix
-#
-# Здесь мы ставим крест на карте.
 { pkgs, ... }:
-
 {
-  # Эта часть правильная.
   systemd.tmpfiles.rules = [
     "d /run/filebrowser 0755 root root -"
     "d /var/lib/filebrowser 0755 root root -"
   ];
-
   virtualisation.oci-containers.containers.filebrowser = {
     image = "filebrowser/filebrowser:latest";
     ports = [ "8088:8080" ];
     volumes = [
-      "/home/alex/Загрузки:/srv:z" # Печать Прохода оставляем, она не мешает.
+      # --- ВОТ ОН, ЭКСПЕРИМЕНТ ---
+      # Мы больше не смотрим на священную землю. Мы смотрим на нейтральную.
+      "/tmp/test_share:/srv:z"
       "filebrowser-data:/config:z"
       "filebrowser-data:/database:z"
     ];
-
-    # --- ВОТ ОНО, ИСТИННОЕ ЛЕКАРСТВО ---
-    # Мы явно приказываем приложению считать своей корневой папкой /srv.
-    cmd = [
-      "--port=8080"
-      "--root=/srv"
-    ];
+    cmd = [ "--port=8080", "--root=/srv" ];
   };
 }
