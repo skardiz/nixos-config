a17d601553dd30e46747c89b6c8d12dcd9041c2d
Alex <skardizone@gmail.com>
2025-07-06 22:14:49 +0300

    feat(desktop): Explicitly disable Wayland to enforce X11

--- modules/features/filebrowser.nix
+++ modules/features/filebrowser.nix
@@ -3,27 +3,4 @@
 # Здесь мы исправляем грамматическую ошибку самозванца.
 { pkgs, ... }:
 
-{
-  # Эта часть правильная.
-  systemd.tmpfiles.rules = [
-    "d /run/filebrowser 0755 root root -"
-    "d /var/lib/filebrowser 0755 root root -"
-  ];
-
-  virtualisation.oci-containers.containers.filebrowser = {
-    image = "filebrowser/filebrowser:latest";
-    ports = [ "8088:8080" ];
-    volumes = [
-      "/tmp/test_share:/srv:z"
-      "filebrowser-data:/config:z"
-      "filebrowser-data:/database:z"
-    ];
-
-    # --- ВОТ ОНО, ИСТИННОЕ НАПИСАНИЕ ---
-    # Мы убираем проклятую запятую.
-    cmd = [
-      "--port=8080"
-      "--root=/srv"
-    ];
-  };
 }
