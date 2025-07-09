cee0bb5f81252ce4baabf52ab195ee2be0909dd0
Alex <skardizone@gmail.com>
2025-07-10 01:05:48 +0300

    ssh sops

--- home/_common/default.nix
+++ home/_common/default.nix
@@ -1,17 +1,21 @@
 # home/_common/default.nix
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, config, ... }: # Убедитесь, что 'config' здесь есть
 
 {
   imports = [
-    inputs.sops-nix.homeManagerModules.sops
-    ./options.nix
-    ./git.nix
+    inputs.sops-nix.homeManagerModules.sops,
+    ./options.nix,
+    ./git.nix,
     ./waydroid-idle.nix
   ];
 
-  # Мы просто сообщаем личному sops-nix, где находится тот же самый системный ключ.
-  # Так как пользователь 'alex' в группе 'sops', у него будут права на чтение.
-  sops.age.keyFile = "/etc/sops/keys/sops.key";
+  # --- ФИНАЛЬНОЕ РЕШЕНИЕ ---
+  # Мы говорим sops-nix использовать ключ, который лежит в вашей домашней директории.
+  # Прямо. Просто. Без копирования. Без костылей.
+  sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+
+  # Мы также указываем, где лежат сами зашифрованные секреты.
+  sops.defaultSopsFile = ../../secrets.yaml;
 
   nixpkgs.config.allowUnfree = true;
   home.stateVersion = "25.11";
--- hosts/shershulya/default.nix
+++ hosts/shershulya/default.nix
@@ -10,37 +10,26 @@
     # ../../modules/features/vpn.nix # Если нужно
   ];
 
-  # Мы учреждаем группу 'sops' для управления доступом к ключу
-  users.groups.sops = {};
+  # Мы полностью убираем все, что связано с sops, из системной конфигурации.
+  # Никаких sops.age.keyFile, sops.secrets, environment.etc, users.groups.sops.
+  # Система больше не занимается секретами. Это задача Home Manager.
 
-  # Мы больше НЕ управляем файлом ключа через environment.etc или activationScripts.
-
-  # Мы просто сообщаем sops-nix, где найти ключ, который мы разместили вручную.
-  sops.age.keyFile = "/etc/sops/keys/sops.key";
-
-  sops.defaultSopsFile = ../../secrets.yaml;
-  sops.secrets = {
-    # vpn_private_key = {};
-    github_token = {}; # neededForUsers здесь больше не нужно, так как нет гонки состояний
-  };
-
-  nix.settings.access-tokens = "github.com=${config.sops.secrets.github_token.path}";
   networking.hostName = "shershulya";
   system.stateVersion = "25.11";
 
+  # В extraGroups для пользователей больше не нужна группа 'sops'.
   my.users.accounts = {
     alex = {
       isMainUser = true;
       description = "Alex";
-      # Даем Алексу доступ к группе 'sops', чтобы его Home Manager мог прочитать ключ
-      extraGroups = [ "adbusers" "sops" ];
+      extraGroups = [ "adbusers" ];
       home = {
         enableUserSshKey = true;
       };
     };
     mari = {
       description = "Mari";
-      extraGroups = []; # Мари НЕ состоит в группе sops
+      extraGroups = [];
       home = {
         enableUserSshKey = false;
       };
